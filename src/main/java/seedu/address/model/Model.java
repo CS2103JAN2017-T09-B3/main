414f1dcb1199fcaeb10a3e299de9711e04b399ba
ivankws <ivan_Kws@hotmail.com>
2017-03-22 23:06:50 +0800

    Update Model.java

--- src/main/java/seedu/address/model/Model.java
+++ src/main/java/seedu/address/model/Model.java
@@ -5,6 +5,7 @@ import java.util.Stack;
 
 import seedu.address.commons.core.Config;
 import seedu.address.commons.core.UnmodifiableObservableList;
+import seedu.address.model.tag.UniqueTagList.DuplicateTagException;
 import seedu.address.model.task.ReadOnlyTask;
 import seedu.address.model.task.Task;
 import seedu.address.model.task.UniqueTaskList;
@@ -14,26 +15,23 @@ import seedu.address.model.task.UniqueTaskList.DuplicateTaskException;
  * The API of the Model component.
  */
 public interface Model {
+	
+	Stack<String> getUndoStack();	
+	Stack<ReadOnlyTask> getDeletedStackOfTasksAdd();
+	Stack<ReadOnlyTask> getDeletedStackOfTasks();
+	Stack<Integer> getDeletedStackOfTasksIndex();
 
-    Stack<String> getUndoStack();
-
-    Stack<ReadOnlyTask> getDeletedStackOfTasksAdd();
-
-    Stack<ReadOnlyTask> getDeletedStackOfTasks();
-
-    Stack<Integer> getDeletedStackOfTasksIndex();
-
-    /**
-     * Clears existing backing model and replaces with the provided new data.
-     */
+	/** Clears existing backing model and replaces with the provided new data. 
+	 * @throws DuplicateTaskException 
+	 * @throws DuplicateTagException */
     void resetData(ReadOnlyAddressBook newData);
 
     /** Returns the AddressBook */
     ReadOnlyAddressBook getAddressBook();
-
+    
     /**Returns the config*/
     Config getConfig();
-
+    
     /** Raises an event to indicate the model has changed */
     void updateFileLocation();
 
@@ -42,35 +40,27 @@ public interface Model {
 
     /** Adds the given task */
     void addTask(Task task) throws UniqueTaskList.DuplicateTaskException;
-
-    /**
-     * Updates the task located at {@code filteredTaskListIndex} with
-     * {@code editedTask}.
-     *
-     * @throws DuplicateTaskException
-     *             if updating the task's details causes the task to be
-     *             equivalent to another existing task in the list.
-     * @throws IndexOutOfBoundsException
-     *             if {@code filteredTaskListIndex} < 0 or >= the size of the
-     *             filtered list.
-     */
-    void updateTask(int filteredTaskListIndex, ReadOnlyTask editedTask) throws UniqueTaskList.DuplicateTaskException;
     
     void revertData();
 
     /**
-     * Returns the filtered task list as an
-     * {@code UnmodifiableObservableList<ReadOnlyTask>}
+     * Updates the task located at {@code filteredTaskListIndex} with {@code editedTask}.
+     *
+     * @throws DuplicateTaskException if updating the task's details causes the task to be equivalent to
+     *      another existing task in the list.
+     * @throws IndexOutOfBoundsException if {@code filteredTaskListIndex} < 0 or >= the size of the filtered list.
      */
+    void updateTask(int filteredTaskListIndex, ReadOnlyTask editedTask)
+            throws UniqueTaskList.DuplicateTaskException;
+
+    /** Returns the filtered task list as an {@code UnmodifiableObservableList<ReadOnlyTask>} */
     UnmodifiableObservableList<ReadOnlyTask> getFilteredTaskList();
 
     /** Updates the filter of the filtered person list to show all persons */
     void updateFilteredListToShowAll();
 
-    /**
-     * Updates the filter of the filtered task list to filter by the given
-     * keywords. Filters in both title and content of task if isInContent is true.
-     * Filters only in title otherwise.
-     */
-    void updateFilteredTaskList(boolean isInContent, Set<String> keywords);
+    /** Updates the filter of the filtered person list to filter by the given keywords*/
+    void updateFilteredTaskList(Set<String> keywords);
+    
+
 }
