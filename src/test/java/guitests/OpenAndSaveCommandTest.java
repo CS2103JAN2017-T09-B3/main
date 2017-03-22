b6ab14e4514cc39d1a16387319ed4b15140c4785
Difeng Tang <difengtang16@hotmail.com>
2017-03-23 01:11:30 +0800

    fix 1.0

--- src/test/java/guitests/OpenAndSaveCommandTest.java
+++ src/test/java/guitests/OpenAndSaveCommandTest.java
@@ -17,32 +17,35 @@ public class OpenAndSaveCommandTest extends AddressBookGuiTest {
     public final String FILE_ALTERNATE_NAME = "data/myPotato";
     public final String FILE_XML_EXTENSION = ".xml";
 
-    //Open and save files functionality
+    // Open and save files functionality
     @Test
     public void save_file() {
-        assertSaveResult("save " + FILE_DIR, FILE_NAME);                     //Default filename: taskmanager.xml
-        assertOpenResult("open " + FILE_DIR, td.getTypicalTasks());                       // File Directory
-        assertSaveResult("save " + FILE_NAME, FILE_NAME);                                 // without .xml
-        assertOpenResult("open " + FILE_NAME, td.getTypicalTasks());                      // without .xml
-        assertSaveResult("save " + FILE_NAME + FILE_XML_EXTENSION, FILE_NAME);            // with .xml
+        assertSaveResult("save " + FILE_DIR, FILE_NAME); // Default filename:
+                                                         // taskmanager.xml
+        assertOpenResult("open " + FILE_DIR, td.getTypicalTasks()); // FileDirectory
+        assertSaveResult("save " + FILE_NAME, FILE_NAME); // without .xml
+        assertOpenResult("open " + FILE_NAME, td.getTypicalTasks()); // without .xml
+        assertSaveResult("save " + FILE_NAME + FILE_XML_EXTENSION, FILE_NAME); // with .xml
         assertOpenResult("open " + FILE_NAME + FILE_XML_EXTENSION, td.getTypicalTasks()); // with .xml
-        assertSaveResult("save " + FILE_ALTERNATE_NAME, FILE_ALTERNATE_NAME); //Ensure successful save, to be used for test cases
+        assertSaveResult("save " + FILE_ALTERNATE_NAME, FILE_ALTERNATE_NAME);
+        // Ensure successful save, to be used for test cases
     }
 
-    //Save file, Delete and Open file to retrieve the saved data
-    //td.getTypicalTasks() = TestTask[]{alice, benson, carl, daniel, elle, fiona}
+    // Save file, Delete and Open file to retrieve the saved data
+    // td.getTypicalTasks() = TestTask[]{alice, benson, carl, daniel, elle,
+    // fiona}
     @Test
     public void save_checkTaskList() {
         assertTrue(taskListPanel.isListMatching(td.getTypicalTasks()));
         assertSaveResult("save " + FILE_NAME, FILE_NAME);
         assertSaveResult("save " + FILE_ALTERNATE_NAME, FILE_ALTERNATE_NAME);
         commandBox.runCommand("delete 1");
-        assertTrue(taskListPanel.isListMatching(new TestTask[]{td.benson, td.carl, td.daniel, td.elle, td.fiona}));
+        assertTrue(taskListPanel.isListMatching(new TestTask[] { td.benson, td.carl, td.daniel, td.elle, td.fiona }));
         assertOpenResult("open " + FILE_NAME + FILE_XML_EXTENSION, td.getTypicalTasks());
         assertTrue(taskListPanel.isListMatching(td.getTypicalTasks()));
     }
 
-    //Test invalid Command confirmation
+    // Test invalid Command confirmation
     @Test
     public void save_invalidCommand_fail() {
         commandBox.runCommand("save" + FILE_DIR);
@@ -51,7 +54,7 @@ public class OpenAndSaveCommandTest extends AddressBookGuiTest {
         assertResultMessage(Messages.MESSAGE_UNKNOWN_COMMAND);
     }
 
-    //Ensure save functions properly
+    // Ensure save functions properly
     private void assertSaveResult(String command, String filename) {
         File testfile = new File(filename + FILE_XML_EXTENSION);
         if (FileUtil.isFileExists(testfile)) {
@@ -61,10 +64,9 @@ public class OpenAndSaveCommandTest extends AddressBookGuiTest {
         assertTrue(FileUtil.isFileExists(testfile));
     }
 
-    //Ensure open functions properly
+    // Ensure open functions properly
     private void assertOpenResult(String command, TestTask... expectedhits) {
         commandBox.runCommand(command);
         assertTrue(taskListPanel.isListMatching(expectedhits));
     }
 }
-
