828645218d4961f907b4e82f31e33254e2e8393e
ivankws <ivan_Kws@hotmail.com>
2017-03-24 00:51:23 +0800

    Update Parser.java

--- src/main/java/seedu/address/logic/parser/Parser.java
+++ src/main/java/seedu/address/logic/parser/Parser.java
@@ -74,11 +74,9 @@ public class Parser {
         case HelpCommand.COMMAND_WORD:
             return new HelpCommand();
 
-        //@@author A0125221Y
         case UndoCommand.COMMAND_WORD:
             return new UndoCommand();
 
-        //@@author
         case SaveCommand.COMMAND_WORD:
             return new SaveCommandParser().parse(arguments);
 
