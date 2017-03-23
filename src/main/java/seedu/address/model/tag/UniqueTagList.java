ba80a318d15bb40899c93e402716ee7ad3e93faf
ivankws <ivan_Kws@hotmail.com>
2017-03-23 21:55:28 +0800

    Update UniqueTagList.java

--- src/main/java/seedu/address/model/tag/UniqueTagList.java
+++ src/main/java/seedu/address/model/tag/UniqueTagList.java
@@ -115,11 +115,13 @@ public class UniqueTagList implements Iterable<Tag> {
                 .filter(tag -> !alreadyInside.contains(tag))
                 .forEach(internalList::add);
     }
-
+    
+    //@@author A0125221Y
     public ObservableList<Tag> getInternalList() {
         return internalList;
     }
-
+    
+    //@@author
     /**
      * Returns true if the list contains an equivalent Tag as the given argument.
      */
