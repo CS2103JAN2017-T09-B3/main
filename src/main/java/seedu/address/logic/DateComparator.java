77ce41d967a73abb411b5809d04581c222b13fe4
vane1992 <e0002761@u.nus.edu>
2017-03-24 13:33:56 +0800

    change

--- src/main/java/seedu/address/logic/DateComparator.java
+++ src/main/java/seedu/address/logic/DateComparator.java
@@ -7,13 +7,13 @@ import seedu.address.model.task.ReadOnlyTask;
 
 public class DateComparator implements Comparator<ReadOnlyTask> {
     public int compareYear(ReadOnlyTask task1, ReadOnlyTask task2) {
-        int year1 = 0, 
+        int year1 = 0,
             year2 = 0;
         if (!task1.getDateTime().isThereEndDateTime()) {
             year1 = 2099;
-        }else if (!task2.getDateTime().isThereEndDateTime()) {
+        } else if (!task2.getDateTime().isThereEndDateTime()) {
             year2 = 2099;
-		}else {
+		} else {
             year1 = task1.getDateTime().endDateTime.getYear();
             year2 = task2.getDateTime().endDateTime.getYear();
         }
@@ -24,48 +24,48 @@ public class DateComparator implements Comparator<ReadOnlyTask> {
             month2 = 0;
         if (!task1.getDateTime().isThereEndDateTime()) {
             month1 = 12;
-        }else if (!task2.getDateTime().isThereEndDateTime()) {
+        } else if (!task2.getDateTime().isThereEndDateTime()) {
             month2 = 12;
-        }else {
+        } else {
             month1 = task1.getDateTime().endDateTime.getMonth();
             month2 = task2.getDateTime().endDateTime.getMonth();
         }
         return Integer.compare(month1, month2);
     }
     public int compareDay(ReadOnlyTask task1, ReadOnlyTask task2) {
-        int day1 = 0, 
+        int day1 = 0,
             day2 = 0;
         if (!task1.getDateTime().isThereEndDateTime()) {
             day1 = 31;
-        }else if (!task2.getDateTime().isThereEndDateTime()) {
+        } else if (!task2.getDateTime().isThereEndDateTime()) {
             day2 = 31;
-        }else {
+        } else {
             day1 = task1.getDateTime().endDateTime.getDate();
             day2 = task2.getDateTime().endDateTime.getDate();
         }
         return Integer.compare(day1, day2);
     }
     public int compareHour(ReadOnlyTask task1, ReadOnlyTask task2) {
-        int hour1 = 0, 
+        int hour1 = 0,
             hour2 = 0;
         if (!task1.getDateTime().isThereEndDateTime()) {
             hour1 = 23;
-        }else if (!task2.getDateTime().isThereEndDateTime()) {
+        } else if (!task2.getDateTime().isThereEndDateTime()) {
             hour2 = 23;
-        }else {
+        } else {
             hour1 = task1.getDateTime().endDateTime.getHour();
             hour2 = task2.getDateTime().endDateTime.getHour();
         }
         return Integer.compare(hour1, hour2);
     }
     public int compareMinute(ReadOnlyTask task1, ReadOnlyTask task2) {
-        int minute1 = 0, 
+        int minute1 = 0,
             minute2 = 0;
         if (!task1.getDateTime().isThereEndDateTime()) {
             minute1 = 59;
-        }else if (!task2.getDateTime().isThereEndDateTime()) {
+        } else if (!task2.getDateTime().isThereEndDateTime()) {
             minute2 = 59;
-        }else {
+        } else {
             minute1 = task1.getDateTime().endDateTime.getMinute();
             minute2 = task2.getDateTime().endDateTime.getMinute();
         }
@@ -78,9 +78,9 @@ public class DateComparator implements Comparator<ReadOnlyTask> {
                 if (compareDay(task1, task2) == 0) {
                     if (compareHour(task1, task2) == 0) {
                         return compareMinute(task1, task2);
-                    }else {return compareHour(task1, task2);}
-                }else {return compareDay(task1, task2);}
-            }else {return compareMonth(task1, task2);}
-        }else {return compareYear(task1, task2);}
+                    } else {return compareHour(task1, task2);}
+                } else {return compareDay(task1, task2);}
+            } else {return compareMonth(task1, task2);}
+        } else {return compareYear(task1, task2);}
     }
 }
