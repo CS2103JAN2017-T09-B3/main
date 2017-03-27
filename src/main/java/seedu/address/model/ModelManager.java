6432fe205107c5e8f9181bfcc1c0ff2474b9a8ac
vane1992 <e0002761@u.nus.edu>
2017-03-27 15:20:02 +0800

    commit

--- src/main/java/seedu/address/model/ModelManager.java
+++ src/main/java/seedu/address/model/ModelManager.java
@@ -238,17 +238,5 @@ public class ModelManager extends ComponentManager implements Model {
             return "name=" + String.join(", ", nameKeyWords);
         }
     }
-
-    //@@author A0135753A
-    @Override
-    public UnmodifiableObservableList<ReadOnlyTask> getDoneTaskList() {
-        FilteredList<ReadOnlyTask> newList = filteredTasks;
-        for (ReadOnlyTask task : newList) {
-            if (!task.getStatus().status) {
-                newList.remove(task);
-            }
-        }
-        return new UnmodifiableObservableList<>(newList);
-    }
-
+    
 }
