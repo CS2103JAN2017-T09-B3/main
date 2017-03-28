0b01ae49a06c645a3eb8a68232c834e87d52bdae
Difeng Tang <difengtang16@hotmail.com>
2017-03-28 14:08:05 +0800

    codacy change

--- src/test/java/guitests/OpenAndSaveCommandTest.java
+++ src/test/java/guitests/OpenAndSaveCommandTest.java
@@ -31,7 +31,7 @@ public class OpenAndSaveCommandTest extends AddressBookGuiTest {
 
     /** Open and save files functionality. */
     @Test
-    public void save_file() {
+    public void SaveAndOpenFile() {
         // Default filename :taskmanager.xml
         assertSaveResult(COMMAND_SAVE + FILE_DIR, FILE_NAME);
         assertOpenResult(COMMAND_OPEN + FILE_DIR, testTask); // FileDirectory
@@ -49,7 +49,7 @@ public class OpenAndSaveCommandTest extends AddressBookGuiTest {
      * location.
      */
     @Test
-    public void save_checkTaskList() {
+    public void SaveAndCheckTaskList() {
         assertTrue(taskListPanel.isListMatching(testTask));
         assertSaveResult(COMMAND_SAVE + FILE_NAME, FILE_NAME);
         assertSaveResult(COMMAND_SAVE + FILE_ALTERNATE_NAME, FILE_ALTERNATE_NAME);
@@ -61,7 +61,7 @@ public class OpenAndSaveCommandTest extends AddressBookGuiTest {
 
     /** Test invalid Command confirmation. */
     @Test
-    public void save_invalidCommand_fail() {
+    public void InvalidCommand() {
         commandBox.runCommand(SaveCommand.COMMAND_WORD + FILE_DIR);
         assertResultMessage(Messages.MESSAGE_UNKNOWN_COMMAND);
         commandBox.runCommand(OpenCommand.COMMAND_WORD + FILE_NAME);
