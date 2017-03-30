c32db55f27a582abf6234079539c8382f492d0da
vane1992 <e0002761@u.nus.edu>
2017-03-30 22:34:35 +0800

    commit

--- src/main/java/seedu/address/logic/commands/MarkAsDoneCommand.java
+++ src/main/java/seedu/address/logic/commands/MarkAsDoneCommand.java
@@ -8,8 +8,9 @@ import seedu.address.commons.core.UnmodifiableObservableList;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.task.ReadOnlyTask;
 import seedu.address.model.task.Status;
-import seedu.address.model.task.UniqueTaskList.DuplicateTaskException;
 import seedu.address.logic.commands.EditCommand.EditTaskDescriptor;
+import seedu.address.model.task.UniqueTaskList.DuplicateTaskException;
+
 
 public class MarkAsDoneCommand extends Command {
 
